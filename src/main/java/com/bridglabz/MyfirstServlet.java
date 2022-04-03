cca5cd9c5cdf6c02b13fb085f648f85c48022e65
susobhankr <susobhan3114@gmail.com>
2022-04-03 18:57:13 +0530

    Added : Create my first Servlet

---
+++ src/main/java/com/bridglabz/MyfirstServlet.java
@@ -0,0 +1,19 @@
+package com.bridglabz;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Hello")
+class MyFirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Hello World My First Servlet</h3>");
+        out.close();
+    }
+}
